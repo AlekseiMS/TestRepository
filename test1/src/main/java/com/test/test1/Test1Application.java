61290db843ae5ac33dddfbad25bd07aa8210fe07
AlekseiSamoilov <99314793+AlekseiMS@users.noreply.github.com>
2022-02-14 13:55:34 +0300

    Added method

--- test1/src/main/java/com/test/test1/Test1Application.java
+++ test1/src/main/java/com/test/test1/Test1Application.java
@@ -1,9 +1,5 @@
 package com.test.test1;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
 public class Test1Application {
 
     public static void main(String[] args) {
@@ -15,8 +11,22 @@ public class Test1Application {
         System.out.println("----");
 
         //В обратном порядке
-        for (int j = 10; j > 0; j--)
-            System.out.println(j);
+        for (int j = 10; j > 0; j--) {
+            //Выведем с помощью метода
+            print(j);
+        }
+        println(""); //Пустая строка
+        println("How are you?");
+    }
+
+    // Пишем метод для вывода int значения в консоли
+    public static void print(int i) {
+        System.out.print(i + " ");
+    }
+
+    //Пишем метод для вывода String значения в консоли
+    public static void println (String s) {
+        System.out.println(s);
     }
 
 }
